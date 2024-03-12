3dc7973381e0db64e91be4162054f70a5202bfc0
Danish Khan <danishk0687@gmail.com>
2024-03-12 19:24:01 +0530

    chnged Chkra link to react-router-dom link in footer and their CSS

--- src/components/Footer/SocialLinks.jsx
+++ src/components/Footer/SocialLinks.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { Flex, Link } from '@chakra-ui/react'
+import { Flex } from '@chakra-ui/react'
+import { Link } from "react-router-dom"
 
 const SocialLinks = () => {
   return (
@@ -7,39 +8,36 @@ const SocialLinks = () => {
       textAlign="center"
       mx={"auto"}
       gap={5}
+      className='socialLinks'
       justifyContent={"center"}
       fontSize={["sm", "md", "lg"]}
       fontWeight={"bold"}
       my={[10, 10]}
     >
       <Link
-        href="https://facebook.com/danishkhan424"
+        to="https://facebook.com/danishkhan424"
         target="_blank"
-        rel="noopener noreferrer"
       >
         Facebook
       </Link>
       |
       <Link
-        href="https://twitter.com/yourpage"
+        to="https://twitter.com/yourpage"
         target="_blank"
-        rel="noopener noreferrer"
       >
         Twitter
       </Link>
       |
       <Link
-        href="https://instagram.com/danish_khan_424"
+        to="https://instagram.com/danish_khan_424"
         target="_blank"
-        rel="noopener noreferrer"
       >
         Instagram
       </Link>
       |
       <Link
-        href="https://linkedin.com/in/danishkhan424"
+        to="https://linkedin.com/in/danishkhan424"
         target="_blank"
-        rel="noopener noreferrer"
       >
         LinkedIn
       </Link>
